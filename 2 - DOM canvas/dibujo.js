27587c8ec66e42f9e8114010f7fc25bb4c65e9d9
Arturoytal <Arturoytal@gmail.com>
2016-10-02 20:54:29 +0200

    linea con lineTo de prueba.

--- 2 - DOM canvas/dibujo.js
+++ 2 - DOM canvas/dibujo.js
@@ -1,1 +1,9 @@
-alert("ola k ase?")
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+lienzo.beginPath();
+lienzo.strokeStyle = "blue";
+lienzo.moveTo(50,100);
+lienzo.lineTo(200,200);
+lienzo.stroke();
+lienzo.closePath();
