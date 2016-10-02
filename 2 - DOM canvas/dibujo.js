213ebca8f9bcfca71804759cf9352c1f5e2f02d0
Arturoytal <Arturoytal@gmail.com>
2016-10-02 22:47:06 +0200

    función dibujarLinea y ciclo while para hacer gráfico.

--- 2 - DOM canvas/dibujo.js
+++ 2 - DOM canvas/dibujo.js
@@ -1,9 +1,25 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
 
-lienzo.beginPath();
-lienzo.strokeStyle = "blue";
-lienzo.moveTo(50,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+while(l < lineas) {
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea("red", 0, yi, xf, 300);
+  console.log("linea " + l)
+  l = l + 1
+}
+
+dibujarLinea("green", 1, 1, 1, 300)
+dibujarLinea("green", 1, 299, 299, 299)
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, xfinal);
+  lienzo.lineTo(yinicial, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
