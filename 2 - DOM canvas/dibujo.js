e8a89f3f86512cd67fc673574827bae39320c4a5
Arturoytal <Arturoytal@gmail.com>
2016-10-07 21:25:08 +0200

    formulario para añadir cuántas líneas quieres

--- 2 - DOM canvas/dibujo.js
+++ 2 - DOM canvas/dibujo.js
@@ -1,19 +1,12 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
 
-while(l < lineas) {
-  xi = 10 * l;
-  yf = 10 * (l + 1);
-  dibujarLinea("red", xi, 300, 0, yf);
-  console.log("linea " + l)
-  l = l + 1
-}
-
-dibujarLinea("green", 1, 300, 1, 1)
-dibujarLinea("green", 299, 299, 1, 299)
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
@@ -22,4 +15,22 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.lineTo(yinicial, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick() {
+var lineas = parseInt(texto.value);
+var l = 0;
+var yi, xf;
+var espacio = ancho / lineas;
+
+while(l < lineas) {
+  xi = espacio * l;
+  yf = espacio * (l + 1);
+  dibujarLinea("red", xi, 300, 0, yf);
+  //console.log("linea " + l)
+  l = l + 1
+}
+
+dibujarLinea("green", 1, 300, 1, 1)
+dibujarLinea("green", 299, 299, 1, 299)
 }
