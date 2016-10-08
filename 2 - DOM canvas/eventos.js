bfaf6b2d6b5f87ed2f469e4bb2f27b15d08bc1a9
Arturoytal <Arturoytal@gmail.com>
2016-10-08 22:32:58 +0200

    Selección o identificación de teclas con switchs en vez de if

--- 2 - DOM canvas/eventos.js
+++ 2 - DOM canvas/eventos.js
@@ -1,3 +1,5 @@
+
+//Variables para teclas de movimiento.
 var tecla = {
   UP: 38,
   DOWN:40,
@@ -5,11 +7,18 @@ var tecla = {
   RIGHT:39
 };
 
-console.log(tecla
-)
+//Ver en la consola el código o identificativo de cada tecla.
+console.log(tecla);
+
+//Escuchamos el teclado a ver qué se pulsa.
 document.addEventListener("keyup", dibujarTeclado);
 
+
+//Función que ejecutará cuando se presionen las teclas de movimiento.
+
 function dibujarTeclado(evento) {
+  //Con if.
+  /*
   if (evento.keyCode == tecla.UP) {
     console.log("Vamo' pa'rriba")
   }
@@ -22,4 +31,24 @@ function dibujarTeclado(evento) {
   if (evento.keyCode == tecla.RIGHT) {
     console.log("Vamo' pa' la derecha")
   }
- }
+  */
+
+  //Con switch
+  switch (evento.keyCode) {
+    case tecla.UP:
+      console.log("Arriba");
+      break;
+    case tecla.DOWN:
+          console.log("Abajo");
+      break;
+    case tecla.LEFT:
+          console.log("Izquierda");
+      break;
+    case tecla.RIGHT:
+          console.log("Derecha");
+      break;
+    default:
+      console.log("Otra tecla!");
+      break;
+  }
+}
